17335855a6970e29a205a21ded410bfad93368cb
FireInstall <feuerreiter@greensurvivors.de>
2025-08-14 17:09:47 +0200

    gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,14 @@ plugins {
     id("java")
 }
 
+val javaVersion = 21
 group = "de.greensurvivors"
 version = "1.0-SNAPSHOT"
+
+java {
+    // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
+    toolchain.languageVersion.set(JavaLanguageVersion.of(javaVersion))
+}
 
 repositories {
     mavenCentral()
